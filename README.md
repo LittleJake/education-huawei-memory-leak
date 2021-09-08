d935039118d687e156aa61ce12dacba055c53c68
LittleJake <465917717@qq.com>
2021-09-08 10:32:54 +0800

    Update
    1. 增加判断API是否为HTTP403，判断掉登录后跳转登录页面。

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# education-huawei-memory-leak
+# 华为云课堂防止内存溢出
 ## 介绍
-解决华为云学院内存溢出问题
-简单处理华为云学院内存溢出的问题，通过删除标签iframe#testIframe，防止video.js一直对#testIframe一直进行刷新导致内存溢出。
+1. **解决华为云学院内存溢出问题**
+   简单处理华为云学院内存溢出的问题，通过删除标签iframe#testIframe，防止video.js一直对#testIframe一直进行刷新导致内存溢出。
+
+2. **增加检测断网后华为云课堂一直刷新，不跳转登录页面**
+
+   通过增加api `POST handler/save_user_video_time`检测是否掉登录的情况。
+
+
 
 ## 问题复现
 [华为云学院视频播放内存溢出问题 - LittleJake's Blog](https://blog.littlejake.net/archives/577/)
@@ -12,3 +18,7 @@
 
 ## Release
 Greasy Fork发布页：[华为云课堂防止内存溢出](https://greasyfork.org/zh-CN/scripts/429240-%E5%8D%8E%E4%B8%BA%E4%BA%91%E8%AF%BE%E5%A0%82%E9%98%B2%E6%AD%A2%E5%86%85%E5%AD%98%E6%BA%A2%E5%87%BA)
+
+## License
+
+Apache2.0
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         华为云课堂防止内存溢出
-// @version      1.0
-// @description  防止video.js刷新iframe导致内存溢出
+// @version      2.0
+// @description  防止video.js刷新iframe导致内存溢出，增加判断登录是否过期。
 // @author       LittleJake
 // @license      Apache2.0
 // @match        https://education.huaweicloud.com/courses/*
@@ -14,4 +14,28 @@
     'use strict';
     // Your code here...
     window.onload = function(){document.getElementById("testIframe").remove();}
+	if ($){
+		let data_id = $(".vert-0").attr("data-id"),
+			course_id = $(".vert-0").children().attr("data-course-id");
+		let ajax_url = "https://education.huaweicloud.com/courses/" + course_id + "/xblock/" + data_id + "/handler/save_user_video_time";
+		setInterval(function(){
+			if ($){
+				$.ajax({
+					url: ajax_url,
+					data: '{"msg":0,"type":""}',
+					method: "POST",
+					error: function(e){
+						console.log(e.status);
+						if (e.status == 403){
+							window.location = "https://auth.huaweicloud.com/authui/login.html?service="+window.location;
+							$ = null;
+						}
+					},
+				});
+			}
+		}, 10000);
+		console.log("===华为云课堂增强脚本===");
+		console.log("项目地址：https://github.com/LittleJake/education-huawei-memory-leak/");
+		console.log("by LittleJake, licensed under Apache2.0");
+	}
 })();
